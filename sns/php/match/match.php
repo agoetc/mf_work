be3508e40275d9afb179138458c22d9d46f38f08
agoetc <b6151@oic.jp>
2017-01-23 11:17:38 +0900

    Add files via upload
    登録している自分のタグの表示

--- sns/php/match/match.php
+++ sns/php/match/match.php
@@ -7,24 +7,33 @@
   }else{
     $tag = '';
   }
-
   if(isset($_SESSION['user_id'])){
     $user_id = $_SESSION['user_id'];
   }else {
-    echo '不正なアクセスです';
+	header('location: ../../');
     die();
   }
-
     try{
       $dsn = 'mysql:dbname=mf_test;host=127.0.0.1';
       $user = 'root';
       $password = '';
       $dbh = new PDO($dsn, $user, $password);
-      //相手のuser_idをもってくる
 
+      //自分が既に登録されてあるか確認
+      $sql = 'select user_id, tag from wait where user_id = ?';
+        $stmt = $dbh->prepare($sql);
+        $flag = $stmt->execute(array($user_id));
+        if(!$flag){
+          echo '接続に失敗しました';
+          die();
+        }
+        if($oldtag = $stmt->fetchColumn(1)){
+          echo '既に登録されています。<br>登録タグ：'.$oldtag.'<br><a href="./match_die.php">取り消す</a><br><br>';
+          die();
+        }
+      //相手のuser_idをもってくる
       $sql = 'SELECT min(wait_id) , user_id FROM wait WHERE tag = ? GROUP BY user_id';
         $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(1, $user_id, PDO::PARAM_STR);
         $flag = $stmt->execute(array($tag));
         if (!$flag){
           echo 'データの取得に失敗しました';
@@ -37,7 +46,7 @@
           //相手がいるとき
         }else {
           //$talk_parとチャットを開始します';
-          include 'match_go.php';
+          	header('location: ./match_go');
           die();
         }
       //user_idをインサート
@@ -49,8 +58,8 @@
         if ($flag){
           echo 'ランダムマッチに登録しました';
         }else{
-          if($talk_par == $user_id){
-            echo '既に登録されています。<a href="./match_die.php">取り消す</a><br><br>';
+          if($talk_par == $user_id || $oldtag == ''){
+            echo '既に登録されています。<br>登録タグ：'.$oldtag.'<br><a href="./match_die.php">取り消す</a><br><br>';
           }else{
             echo '登録に失敗しました';
           }
